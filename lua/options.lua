31ea42f67581364e7c836bda05579fa136c10c7f
dp997 <dominik.pierzga@gmail.com>
2025-03-28 20:26:48 +0100

    add folding

--- lua/options.lua
+++ lua/options.lua
@@ -16,3 +16,13 @@ opt.completeopt = "menuone,noinsert,noselect" -- Autocomplete options
 opt.textwidth = 0
 opt.wrapmargin = 0
 opt.colorcolumn = "80"
+
+-- folding
+opt.foldmethod = "expr"
+opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+opt.foldcolumn = "0"
+opt.foldtext = ""
+opt.foldlevel = 99
+opt.foldlevelstart = 1
+opt.foldnestmax = 4
